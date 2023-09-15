6b1a578d07fb4e79dba17645dc5db18140b1cf2c
andriytol <andriyytolstoy@gmail.com>
2023-09-15 08:33:10 +0300

    init project

---
+++ server.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const app = require('./app')
+
+const DB_HOST = 'mongodb+srv://Andriy:roLKF2rQHH7Xlpje@cluster0.l2xsudc.mongodb.net/books_reader?retryWrites=true&w=majority'
+
+mongoose.set('strictQuery', true)
+
+mongoose.connect(DB_HOST)
+    .then(() => app.listen(3000, () => console.log('Database connect success')))
+    .catch(error => {
+        console.log(error.message)
+        process.exit(1)
+    })
