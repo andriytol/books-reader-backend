cd8aeef1ad250513ea5952537e25d15d9833f480
andriytol <andriyytolstoy@gmail.com>
2023-09-15 12:15:37 +0300

    move DB_HOST to process.env

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose')
 
 const app = require('./app')
 
-const DB_HOST = 'mongodb+srv://Andriy:roLKF2rQHH7Xlpje@cluster0.l2xsudc.mongodb.net/books_reader?retryWrites=true&w=majority'
+const { DB_HOST } = process.env
+
+// const DB_HOST = 'mongodb+srv://Andriy:roLKF2rQHH7Xlpje@cluster0.l2xsudc.mongodb.net/books_reader?retryWrites=true&w=majority'
 
 mongoose.set('strictQuery', true)
 
